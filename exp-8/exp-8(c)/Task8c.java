3185f03c0c06a7c32677f0c36ade5dd6880057a0
Sudharshan Reddy <244g1a05cs@srit.ac.in>
2026-03-14 13:31:38 +0530

    Added new programs

---
+++ exp-8/exp-8(c)/Task8c.java
@@ -0,0 +1,42 @@
+class ArithmeticOperations
+{
+    int addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    int subtraction(int a, int b)
+    {
+        return a - b;
+    }
+
+    int multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    int division(int a, int b)
+    {
+        return a / b;
+    }
+}
+
+class Task8c
+{
+    public static void main(String args[])
+    {
+        ArithmeticOperations ao = new ArithmeticOperations();
+
+        int sum = ao.addition(10,5);
+        System.out.println("Addition: " + sum);
+
+        int diff = ao.subtraction(10,5);
+        System.out.println("Subtraction: " + diff);
+
+        int prod = ao.multiplication(10,5);
+        System.out.println("Multiplication: " + prod);
+
+        int quot = ao.division(10,5);
+        System.out.println("Division: " + quot);
+    }
+}
